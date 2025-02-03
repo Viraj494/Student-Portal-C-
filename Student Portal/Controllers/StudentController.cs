ca06986ac6cec65387eabc8315f54d6a35d99242
Viraj494 <virajkalhara1234@gmail.com>
2025-02-03 11:17:11 +0530

    Add project files.

---
+++ Student Portal/Controllers/StudentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Portal.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ Student Portal/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Portal.Models.Entities;
+
+namespace Student_Portal.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
